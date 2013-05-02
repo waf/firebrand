c04b3e0c393334c35553ca6ba0d077e4711e67d8
Will Fuqua <wafuqua@gmail.com>
2013-05-02 01:14:09 -0400

    Create IrcBot wrapper around IrcConnection
    Use this IrcBot wrapper in new Firebrand main method

---
+++ Firebrand/Firebrand.fs
@@ -0,0 +1,17 @@
+﻿module Firebrand.Main
+
+open Firebrand.IrcBot
+
+let server = "irc.freenode.net"
+let port = 6667
+let nick = "firebrand"
+let user = "firebrand"
+let channel = "#firebrand-test"
+
+[<EntryPoint>]
+let main args = 
+    let nickInfo = {nick = nick; username = user; realname = user}
+    let bot = IrcBot(server, port, nickInfo)
+    bot.Join(channel)
+    bot.Listen()
+    0
---
+++ Firebrand/IrcBot.fs
@@ -0,0 +1,31 @@
+﻿module Firebrand.IrcBot
+
+open System
+open Firebrand.IrcCommand.Outbound
+open Firebrand.IrcCommand.Inbound
+open Firebrand.IrcConnection
+
+type BotName = { nick : String; username : String; realname : String }
+
+type IrcBot(server, port, name) = 
+
+    let conn = IrcConnection(server, port)
+    do    
+        conn.Send(Nick(name.nick))
+        conn.Send(User(name.username, name.realname))
+
+    member this.Join(channel) = 
+        conn.Send(Join(channel))
+
+    member this.Listen() = 
+        for cmd in conn.Stream() do
+            match this.respond cmd with
+                | Some(response) -> conn.Send response
+                | None -> ()
+
+    member this.respond cmd =
+        match cmd with
+            | Ping server -> Some(Pong server)
+            | ChannelMessage (channel, msg) -> Some(OutMessage(channel, "You said " + msg))
+            | UserMessage (user, msg) -> Some(OutMessage(user, "You said " + msg))
+            | _ -> None
